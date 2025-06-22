962f9f2d04158e1ab316a43ac11e9ecd18495fed
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2025-06-22 11:08:12 +0700

    Update push2db-send2tele.py

--- push2db-send2tele.py
+++ push2db-send2tele.py
@@ -73,7 +73,7 @@ class JadwalShalat:
 📿 "{jadwal[9]}"
 ------------------
 *Laporan Traffic Sensor*
-📊 Jumlah Data: {count} records
+📊 Jumlah Data {count} records
 """
                 requests.post(self.uri_telegram, json={'msg': message})
                 print("Message sent to Telegram.")
