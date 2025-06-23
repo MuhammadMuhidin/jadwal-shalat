624e796f9968539e3a149bbe7c464566fa29088a
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2025-06-23 19:55:29 +0700

    Update push2db-send2tele.py

--- push2db-send2tele.py
+++ push2db-send2tele.py
@@ -62,7 +62,7 @@ class JadwalShalat:
                 for conn in [self.db, self.db_dr]:
                     with psycopg2.connect(conn) as c, c.cursor() as curr:
                         curr.execute('select count(*) from realtime_traffic_sensor')
-                        totals.append(cur.fetchone()[0])
+                        totals.append(curr.fetchone()[0])
                 status_check='OK' if totals[0]==totals[1] else 'BAD'
             except Exception as e:
                 status_check=f'Error: {e}'
