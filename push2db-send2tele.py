b1056c89a86624dbf0e19b726e8cebae1cc3e9c1
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2025-06-23 20:09:50 +0700

    Update push2db-send2tele.py

--- push2db-send2tele.py
+++ push2db-send2tele.py
@@ -52,8 +52,6 @@ class JadwalShalat:
             cursor = conn.cursor()
             cursor.execute("SELECT * FROM jadwal_shalat ORDER BY TO_DATE(SPLIT_PART(tanggal,',',2),'DD/MM/YYYY') DESC LIMIT 1")
             jadwal = cursor.fetchone()
-            cursor.execute("SELECT COUNT(*) FROM realtime_traffic_sensor")
-            count = cursor.fetchone()[0]
             cursor.close()
             conn.close()
 
